0c608cafefc5c758f6835fd49ac7ae85aed81b8a
Dominic Mushayi <mushayidominic@gmail.com>
2025-01-23 17:12:54 +0200

    Update

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -75,7 +75,7 @@ const Header: FC = () => {
     <header>
       {/* Navigation Overlay */}
       <div
-        className="fixed top-0 left-0 w-full h-0 overflow-hidden bg-stone-900"
+        className="fixed top-0 left-0 w-full h-0 overflow-hidden bg-stone-900 z-10"
         ref={navScope}
       >
         <nav className="mt-20 flex flex-col">
@@ -112,10 +112,10 @@ const Header: FC = () => {
       </div>
 
       {/* Header Top Bar */}
-      <div className="fixed top-0 left-0 w-full mix-blend-difference backdrop-blur-md">
+      <div className="fixed top-0 left-0 w-full mix-blend-difference backdrop-blur-md z-10">
         <div className="container !max-w-full">
           <div className="flex justify-between h-20 items-center">
-            <a href="/">
+            <a href="#">
               <span className="text-3xl font-bold uppercase text-white sm:text-white">
                 M&nbsp;D&nbsp;L
               </span>
@@ -125,7 +125,7 @@ const Header: FC = () => {
       </div>
 
       {/* Menu Button and Contact Button */}
-      <div className="fixed top-0 left-0 w-full">
+      <div className="fixed top-0 left-0 w-full z-10">
         <div className="container !max-w-full">
           <div className="flex justify-end h-20 items-center">
             <div className="flex items-center gap-4">
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,16 +1,31 @@
 "use client";
 
-import { FC, useEffect } from "react";
+import { FC, MouseEvent ,useEffect, useRef, useState } from "react";
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 import heroImage from "@/assets/images/logo-color.png";
 import Image from "next/image";
 import Button from "@/components/Button";
-import { motion, useAnimate } from "motion/react";
+import {
+  motion,
+  useAnimate,
+  stagger,
+  useScroll,
+  useTransform,
+} from "motion/react";
 import SplitType from "split-type";
-import { stagger } from "motion";
 
 const Hero: FC = () => {
   const [titleScope, titleAnimate] = useAnimate();
+  const scrollingDiv = useRef<HTMLDivElement>(null);
+    const [isOpen, setIsOpen] = useState(false);
+  
+  const { scrollYProgress } = useScroll({
+    target: scrollingDiv,
+    offset: ["start end", "end end"],
+  });
+
+  const portraitWidth = useTransform(scrollYProgress, [0, 1], ["100%", "240%"]);
+
   useEffect(() => {
     new SplitType(titleScope.current, {
       types: "lines,words",
@@ -29,9 +44,24 @@ const Hero: FC = () => {
     );
   }, []);
 
+    const handleClickNavItem = (e:MouseEvent<HTMLAnchorElement>) => {
+      e.preventDefault();
+      setIsOpen(false);
+      const url = new URL(e.currentTarget.href);
+      const hash = url.hash;
+      const target = document.querySelector(hash);
+  
+      if (!target) return;
+      target.scrollIntoView({ behavior: "smooth" });
+      
+  
+      console.log(hash); 
+      
+    };
+
   return (
-    <section className="sm:">
-      <div className="grid md:grid-cols-12 md:h-screen items-stretch ">
+    <section>
+      <div className="grid md:grid-cols-12 md:h-screen items-stretch sticky top-0 ">
         <div className="md:col-span-7 flex flex-col justify-center">
           <div className="sm:section container !max-w-full">
             <motion.h1
@@ -52,45 +82,52 @@ const Hero: FC = () => {
                   delay: 2,
                 }}
               >
-                <Button
-                  variant="secondary"
-                  iconAfter={
-                    <div className="overflow-hidden size-5">
-                      <div className="h-5 w-10 flex group-hover/button:-translate-x-1/2 transition-transform duration-500 ">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="size-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-                          />
-                        </svg>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="size-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                  }
+                <a
+                  href="#projects"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={handleClickNavItem}
                 >
-                  <span>View Our Work</span>
-                </Button>
+                  <Button
+                    variant="secondary"
+                    iconAfter={
+                      <div className="overflow-hidden size-5">
+                        <div className="h-5 w-10 flex group-hover/button:-translate-x-1/2 transition-transform duration-500 ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-5"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                            />
+                          </svg>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-5"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+                    }
+                  >
+                    <span>View Our Work</span>
+                  </Button>
+                </a>
               </motion.div>
               <motion.div
                 initial={{ opacity: 0, y: "100%" }}
@@ -100,21 +137,34 @@ const Hero: FC = () => {
                   delay: 2.4,
                 }}
               >
-                <Button variant="text">Let&apos;s Talk</Button>
+                <a
+                  href="https://wa.me/263711314881"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  
+                >
+                  <Button variant="text">Let&apos;s Talk</Button>
+                </a>
               </motion.div>
             </div>
           </div>
         </div>
-        <div className="md:col-span-5">
-          <div className="mt-20 md:mt-0 md:h-full">
+        <div className="md:col-span-5 relative">
+          <motion.div
+            className="mt-20 md:mt-0 md:size-full md:absolute md:right-0 max-md:w-full "
+            style={{
+              width: portraitWidth,
+            }}
+          >
             <Image
               src={heroImage}
               alt="hero"
               className="size-full object-cover"
             />
-          </div>
+          </motion.div>
         </div>
       </div>
+      <div className="md:h-[200vh] " ref={scrollingDiv}></div>
     </section>
   );
 };
